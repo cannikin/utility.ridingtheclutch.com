eb2a21b9948766f4e3b6ef0e55eae1b1098f74b9
Rob Cameron <cannikinn@gmail.com>
2013-08-05 16:55:12 +0000

    Adds debug URL, ping URL

--- app.rb
+++ app.rb
@@ -3,20 +3,35 @@ require 'aws/ses'
 
 configure do
   enable :logging
-  set :ses, AWS::SES::Base.new(:access_key_id => 'AKIAJKCSLTTTQDBOB5KQ', 
-                               :secret_access_key => '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')
+  set :ses, AWS::SES::Base.new(:access_key_id => ENV['ACCESS_KEY'], 
+                               :secret_access_key => ENV['SECRET_KEY'])
+end
+
+get '/ping' do
+  'pong'
+end
+
+get '/debug' do
+    settings.ses.send_email :to        => ["cannikinn@gmail.com"],
+                            :source    => %Q{"debug" <sales@#{params[:domain] || 'precinstr.com'}>},
+                            :reply_to  => 'sales@precinstr.com',
+                            :subject   => 'Email successful.',
+                            :text_body => 'Email was sent (obviously)'
+
+    "ok"
 end
 
 post '/' do
   begin
-    settings.ses.send_email :to        => ['sales@precinstr.com'],
-                            :source    => %Q{"#{params[:from]}" <sales@precinstr.com>},
+    settings.ses.send_email :to        => ["sales@#{params[:domain] || 'precinstr.com'}"],
+                            :source    => %Q{"#{params[:from]}" <sales@#{params[:domain] || 'precinstr.com'}>},
                             :reply_to  => params[:from],
                             :subject   => params[:subject],
                             :text_body => params[:message]
  
     redirect request.referrer.split('?').first + '?sent=true'
-  rescue
+  rescue => e
+    puts e.inspect
     redirect request.referrer.split('?').first + '?message=<strong>There was a problem...</strong><br>Are you sure you entered a valid email address?'
   end
 end
